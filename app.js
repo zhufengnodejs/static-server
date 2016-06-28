f061b3bf90907174ee066c7e61cdbe55f1153e13
zhufengnodejs <zhufengnodejs@126.com>
2016-06-28 16:10:31 +0800

    5.处理缓存

--- app.js
+++ app.js
@@ -6,24 +6,87 @@ var mime = require('mime');
 
 http.createServer(function (request, response) {
     var pathname = url.parse(request.url).pathname;
+    console.log(pathname);
     var filename = path.join(__dirname, 'public', pathname);
     fs.exists(filename, function (exists) {
-        if (!exists) {
-            response.writeHead(404, {'Content-Type': 'text/plain'});
-            response.write("请求的URL " + pathname + " 在服务器上不存在!");
-            response.end();
+        if (exists) {
+            byEtag();
         } else {
-            fs.readFile(filename, function (err, file) {
-                if (err) {
-                    response.writeHead(500, {'Content-Type': 'text/plain'});
-                    response.end(err);
-                } else {
-                    response.writeHead(200, {'Content-Type': mime.lookup(filename)});
-                    response.write(file);
-                    response.end();
-                }
-
-            });
+            sendNotFound();
         }
     })
+
+    function byEtag() {
+        var ifNoneMatch = request.headers['if-none-match'];
+        if (ifNoneMatch) {
+            fs.readFile(filename, function (err, file) {
+                if (err) {
+                    sendServerError();
+                } else {
+                    var etag = require('crypto').createHash('md5').update(file).digest('hex');
+                    if (etag == ifNoneMatch) {
+                        byTime();
+                    } else {
+                        sendFile(undefined,etag,file);
+                    }
+                }
+            });
+        } else {
+            byTime();
+        }
+    }
+
+    function byTime() {
+        var ifModifiedSince = request.headers['if-modified-since'];
+        fs.stat(filename, function (err, stat) {
+            if (ifModifiedSince == stat.mtime.toUTCString()) {
+                sendNotModified();
+            } else {
+                sendFile(stat.mtime.toUTCString());
+            }
+        })
+    }
+
+    function sendNotFound() {
+        response.writeHead(404, {'Content-Type': 'text/plain'});
+        response.end('Not Found');
+    }
+
+    function sendServerError() {
+        response.writeHead(500, {'Content-Type': 'text/plain'});
+        response.end(err);
+    }
+
+    function sendNotModified() {
+        response.writeHead(304, {'Content-Type': 'text/plain'});
+        response.end('Not Modified');
+    }
+
+    function sendFile(LastModified, etag, file) {
+        if (LastModified && etag) {
+            send(LastModified, etag, file);
+        } else if (etag) {
+            fs.stat(filename, function (err, stat) {
+                send(stat.mtime.toUTCString(), etag, file);
+            })
+        } else if (LastModified) {
+            fs.readFile(filename, function (err, file) {
+                send(LastModified, require('crypto').createHash('md5').update(file).digest('hex'), file);
+            });
+        }
+    }
+
+    function send(etag, LastModified, file) {
+        var expires = new Date(Date.now() + 3600 * 1000);
+        response.writeHead(200, {
+            'Cache-Control': 'Max-Age=3600',
+            'Expires': expires.toUTCString(),
+            'Content-Type': mime.lookup(filename),
+            'Last-Modified': LastModified,
+            'Etag': etag
+        });
+        response.write(file);
+        response.end();
+    }
 }).listen(8888);
+
